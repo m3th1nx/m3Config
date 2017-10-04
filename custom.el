907fe4da6b7b6c90b0497dd21950fd9c46dcedb3
m3th1nx <methinx@gmail.com>
2017-10-04 09:09:13 -0400

    set send mail via customize-group

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(global-hl-line-mode t))
+ '(global-hl-line-mode t)
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
